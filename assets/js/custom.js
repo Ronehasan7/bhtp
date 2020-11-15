6425f43a0fc45bf536bd3b67324b5d0ef5f65fa1
Ronehasan7 <43841823+Ronehasan7@users.noreply.github.com>
2020-11-15 17:07:13 +0600

    Update
    Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -54,3 +54,62 @@ var TxtRotate = function(el, toRotate, period) {
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
   };
+
+
+
+/*
+    Carousel
+*/
+var breakpoint = {
+  // Small screen / phone
+  sm: 576,
+  // Medium screen / tablet
+  md: 768,
+  // Large screen / desktop
+  lg: 992,
+  // Extra large screen / wide desktop
+  xl: 1200
+};
+
+// slick slider
+$('#slick').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  draggable: true,
+  infinite: true,
+  dots: false,
+  arrows: false,
+  speed: 1000,
+  mobileFirst: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [{
+      breakpoint: breakpoint.sm,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: breakpoint.md,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: breakpoint.lg,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: breakpoint.xl,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
