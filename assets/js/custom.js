a8acc8577e33cdaab779a06fa94aa25df1c9bb97
Ronehasan7 <43841823+Ronehasan7@users.noreply.github.com>
2020-11-16 11:57:40 +0600

    up icon

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,25 +6,19 @@ var TxtRotate = function(el, toRotate, period) {
     this.txt = '';
     this.tick();
     this.isDeleting = false;
-  };
-  
+  };  
   TxtRotate.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
+    var fullTxt = this.toRotate[i];  
     if (this.isDeleting) {
       this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
       this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
+    }  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';  
     var that = this;
-    var delta = 300 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
+    var delta = 300 - Math.random() * 100;  
+    if (this.isDeleting) { delta /= 2; }  
     if (!this.isDeleting && this.txt === fullTxt) {
       delta = this.period;
       this.isDeleting = true;
@@ -32,8 +26,7 @@ var TxtRotate = function(el, toRotate, period) {
       this.isDeleting = false;
       this.loopNum++;
       delta = 500;
-    }
-  
+    }  
     setTimeout(function() {
       that.tick();
     }, delta);
@@ -54,6 +47,27 @@ var TxtRotate = function(el, toRotate, period) {
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
   };
+/*== Typing end ==*/
+
+// Back to top button   
+  //Get the button
+  var mybutton = document.getElementById("backTop");
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
 
 
 
